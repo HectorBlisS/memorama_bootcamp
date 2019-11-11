9d4bf3e63fc5f8466aed1b8a806225fc07240fef
Fixter <fixter@MacBook-Pro-de-Fixter.local>
2019-11-11 13:06:09 -0600

    done

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 // constantes
 const URL = "https://rickandmortyapi.com/api/character/"
+let height = 0
 // selectores
 let ignition = document.querySelector('#ignition')
 let container = document.querySelector('.container')
-
-//add
-let height = 0
 
 // function
 function getData() {
@@ -21,26 +19,15 @@ function getData() {
         })
 }
 function displayData(name, image) {
-    // 1.- crear un nodo
     let div = document.createElement('div')
     div.className = "character"
     div.innerHTML = `<img src="${image}" /> <p>${name}</p>`
-    // 2.- vamos a manipular al padre
-    // 3.- vamos a insertar el nodo en el padre
-
-
-
-    // add
-    div.addEventListener("click", ()=>{
-        height -= 460
-        container.style.transform = `translateY(${height}px)`
-        console.log(height)
-        if(height<-8644) {
-            height = 460
-        }
-    })
-
     container.appendChild(div)
+    div.addEventListener('click', e=>{
+        height += 460
+        if(height>9000) height = 0
+        container.style.transform = `translateY(-${height}px)`
+    })
 
 }
 
